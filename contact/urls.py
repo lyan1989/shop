eb323573362b5c7bb246b05480e4e0140de85ee6
lyan <lyan1989@163.com>
2019-06-12 20:02:47 +0800

    adjust the product details page

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
 
     url(r'^success/', views.successView, name='send_email_success'),
     url(r'^email/', views.emailView, name='email'),
+    url(r'^$', views.emailView, name='contact_us'),
 
 ]
--- contact/views.py
+++ contact/views.py
@@ -34,4 +34,3 @@ def successView(request):
     return render(request, 'contact/success.html')
 
 
-
