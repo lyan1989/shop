3a19a780e09f0e50e624b8f5e3f1677883071dea
lyan <lyan1989@163.com>
2019-06-18 20:31:43 +0800

    update

--- fork_app/catalogue/models.py
+++ fork_app/catalogue/models.py
@@ -13,9 +13,59 @@ class Product(AbstractProduct):
     tutorial = models.TextField(blank=True)
     aliexpress_url = models.URLField(blank=True)
 
-    def calculate_rating(self):
-        results
+    @property
+    def num_5stars(self):
+        return self.reviews.filter(
+            status=self.reviews.model.APPROVED, score=5
+        ).count()
+    @property
+    def percent_5stars(self):
+        return (100 * self.num_5stars) / self.reviews.all().count()
+
+    @property
+    def num_4stars(self):
+        return self.reviews.filter(
+            status=self.reviews.model.APPROVED, score=4
+        ).count()
+    @property
+    def percent_4stars(self):
+        return (100 * self.num_4stars) / self.reviews.all().count()
+
+    @property
+    def num_3stars(self):
+        return self.reviews.filter(
+            status=self.reviews.model.APPROVED, score=3
+        ).count()
+    @property
+    def percent_3stars(self):
+        return (100 * self.num_3stars) / self.reviews.all().count()
+
+    @property
+    def num_2stars(self):
+        return self.reviews.filter(
+            status=self.reviews.model.APPROVED, score=2
+        ).count()
+    @property
+    def percent_2stars(self):
+        return (100 * self.num_2stars) / self.reviews.all().count()
+
+    @property
+    def num_1stars(self):
+        return self.reviews.filter(
+            status=self.reviews.model.APPROVED, score=1
+        ).count()
+    @property
+    def percent_1stars(self):
+        return (100 * self.num_1stars) / self.reviews.all().count()
+
+
+
+
+class Category(AbstractCategory):
+
+    @property
+    def last_name(self):
+        names = [category.name for category in self.get_ancestors_and_self()]
+        return names[1]
 
 from oscar.apps.catalogue.models import *
-
-
